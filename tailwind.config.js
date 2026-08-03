4c261fb7fed9e67b4c38699f69f33097066fa160
minami <rouet76apt.56@gmail.com>
2026-08-03 16:16:13 +0900

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resource/**/*.js",
+    "./resource/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
